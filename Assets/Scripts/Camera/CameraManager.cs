b8fc099602a855ce9edeab9067a89e689492249f
HideousArmor <manubare@gmail.com>
2023-12-02 16:26:54 +0100

    Camera & scenario prefabs

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -8,12 +8,12 @@ public class CameraManager : MonoBehaviour
     [SerializeField] Transform[] focusObjects;
 
     [Header("Zoom")]
-    [SerializeField, Tooltip("Cam z pos when players are furthest apart")] float maxZPos = 0;
-    [SerializeField, Tooltip("Cam z pos when players are closest")] float minZPos = 40;
-    [SerializeField, Tooltip("Margin on the side of each player")] float hMargin = 40;
+    [SerializeField, Tooltip("Cam z pos when players are furthest apart")] float maxZPos = -40;
+    [SerializeField, Tooltip("Cam z pos when players are closest")] float minZPos = 5;
+    [SerializeField, Tooltip("Margin on the side of each player")] float hMargin = 10;
     [SerializeField] AnimationCurve zPosCurve;
-    [SerializeField, Tooltip("Max Distance possible between players")] float maxXDistance = 180;
-    [SerializeField, Tooltip("Min Distance possible between players")] float minXDistance = 130;
+    [SerializeField, Tooltip("Max Distance possible between players")] float maxXDistance = 80;
+    [SerializeField, Tooltip("Min Distance possible between players")] float minXDistance = 15;
 
     [Header("Misc"), Space(20)]
     [SerializeField] RectTransform foreGroundLayer;
@@ -40,7 +40,7 @@ public class CameraManager : MonoBehaviour
         Vector3 furthestLeft = focusObjects[0].position;
         Vector3 furthestRight = focusObjects[1].position;
         Vector3 finalCameraPos = transform.localPosition;
-        Vector3 finalForegroundPos = foreGroundLayer.localPosition;
+        //Vector3 finalForegroundPos = foreGroundLayer.localPosition;
 
         foreach (Transform t in focusObjects)
         {
@@ -61,10 +61,11 @@ public class CameraManager : MonoBehaviour
         focusCenter = focusCenter / focusObjects.Length;
         focusCenter.x = Mathf.Clamp(focusCenter.x, -maxXPos, maxXPos);
         xPosRatio = Mathf.InverseLerp(-maxXPos, maxXPos, focusCenter.x);
+        /*
         finalForegroundPos.z = Mathf.Lerp(foregroundMinZPos, foregroundMaxZPos, distanceRatio);
         finalForegroundPos.x = Mathf.Lerp(foregroundMaxXPos, -foregroundMaxXPos, xPosRatio);
         foreGroundLayer.localPosition = finalForegroundPos;
-
+        */
         finalCameraPos.x = Mathf.Lerp(focusCenter.x, 0, distanceRatio);
         finalCameraPos.z = Mathf.Lerp(minZPos, maxZPos, zPosCurve.Evaluate(distanceRatio));
         transform.localPosition = Vector3.Lerp(transform.localPosition, finalCameraPos, Time.deltaTime * trackingSpeed);
