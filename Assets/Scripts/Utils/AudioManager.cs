d672a304d23c6e474eca035e5b3af017a2a496d5
HideousArmor <manubare@gmail.com>
2023-12-03 11:00:36 +0100

    fix duplicated audioManager

--- Assets/Scripts/Utils/AudioManager.cs
+++ Assets/Scripts/Utils/AudioManager.cs
@@ -24,7 +24,7 @@ public class AudioManager : MonoBehaviour
         // If there is an instance, and it's not me, delete myself.
         if (Instance != null && Instance != this)
         {
-            Destroy(this);
+            Destroy(this.gameObject);
         }
         else
         {
