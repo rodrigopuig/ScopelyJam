dde462c2900c663795d241384877eae2ea4be388
rodrigopuig <rodrigopuig94@gmail.com>
2023-12-03 01:17:50 +0100

    finish

--- Assets/Dani/Player.cs
+++ Assets/Dani/Player.cs
@@ -214,10 +214,10 @@ public class Player : MonoBehaviour
         }
 
         if (cargo.eulerAngles.z > 270 || cargo.eulerAngles.z < 90)
-        { /*
+        { 
             Debug.Log("GAME OVER");
             Time.timeScale = 0.00001f;
-            GameController.instance.NextRound(this);*/
+            GameController.instance.NextRound(this);
         }
     }
 
--- Assets/Rodrigo/Scripts/UI/FinishGame.cs
+++ Assets/Rodrigo/Scripts/UI/FinishGame.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 public class FinishGame : MonoBehaviour
 {
 
+    public GameObject player1_win, player1_lose, player2_win, player2_lose;
+
     bool inputAllowed;
 
     private void Awake()
     {
         inputAllowed = true;
+
+        if(PlayerPrefs.GetInt(DataUtils.playerScore1) == 1)
+        {
+            player1_win.SetActive(true);
+            player2_win.SetActive(false);
+            player1_lose.SetActive(false);
+            player2_lose.SetActive(true);
+        }
+        else
+        {
+            player1_win.SetActive(false);
+            player2_win.SetActive(true);
+            player1_lose.SetActive(true);
+            player2_lose.SetActive(true);
+        }
     }
 
     public void Replay()
